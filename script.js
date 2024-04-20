e2b7134dbae1cc7e3cb6d6d037d4a625f8a45322
hanzalahsamana <hanzalahsamana789@gmail.com>
2024-04-20 12:14:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateAge(dob) {
+  var currentDate = new Date();
+  var dobDate = new Date(dob);
+
+  var age = currentDate.getFullYear() - dobDate.getFullYear();
+  var monthDiff = currentDate.getMonth() - dobDate.getMonth();
+
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && currentDate.getDate() < dobDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+var dob = prompt("Please enter your date of birth (YYYY-MM-DD):");
+var age = calculateAge(dob);
+alert("Your age is: " + age);
